04bb89561b9a24615aa5b735154d03de5b91818d
ata-dotcom <97622126+ata-dotcom@users.noreply.github.com>
2022-01-13 00:09:03 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "vm" {
-  ami           = "DUMMY_VALUE_AMI_ID"
-  subnet_id     = "DUMMY_VALUE_SUBNET_ID"
+  ami           = "Your_AMI_ID"
+  subnet_id     = "Your_SUBNET_ID"
   instance_type = "t3.micro"
   tags = {
     Name = "my-first-tf-node"
